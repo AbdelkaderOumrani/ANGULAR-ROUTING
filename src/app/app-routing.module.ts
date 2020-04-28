6fab5d51041fe135c7f7ef5c6c6e5f35c1cba0f6
AbdelkaderOumrani <47723408+AbdelkaderOumrani@users.noreply.github.com>
2020-04-28 03:02:45 +0100

    Product Component Finished

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'products',
+  component: ProductsComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
